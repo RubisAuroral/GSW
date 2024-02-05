91f3241eb3112c74fe668f0732135a111c21115d
Alan <Azanith1610@gmail.com>
2024-02-05 10:35:34 +0100

    allCapteur+generator

---
+++ Raspberry/Capteurs/capteur7.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+while true; do
+    data=$(sudo tcpdump -i wlan1mon -e subtype probe-req -c 1 2>/dev/null)
+    mac=$(echo "$data" | cut -d" " -f13 | cut -d ":" -f2-)
+    echo "$data" | awk '{print "7;" $1 ";" $7 ";" $13}' | nc -w0 192.168.43.52 12345
+done
